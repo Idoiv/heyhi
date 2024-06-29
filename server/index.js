9e1d01a1b184b3b05e70377c782830e7c315f79d
idoivry <t-idoivry@microsoft.com>
2024-06-29 17:11:30 +0300

    t

--- server/index.js
+++ server/index.js
@@ -15,4 +15,13 @@ app.use('/api', require('./routes/api'));
 
 const PORT = process.env.PORT || 5000;
 
+
+app.get('/health', (req, res) => {
+    res.status(200).send('OK');
+  });
+  
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
