ea4b9a8060df302edc145312b4b950e571d77585
idoivry <t-idoivry@microsoft.com>
2024-07-01 01:56:41 +0300

    login

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.get('/health', (req, res) => {
     res.status(200).send('OK');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
